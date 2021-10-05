f52ac182b91d6a240cfdd70b9b06ffff352979aa
sghadai2997 <89852365+sghadai2997@users.noreply.github.com>
2021-10-04 22:31:08 -0400

    Update loopTable.php

--- Assignment2/loopTable.php
+++ Assignment2/loopTable.php
@@ -7,13 +7,6 @@
 
 
 
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +14,7 @@
 	<meta http-equiv="X-UA-Compatible" content ="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-	<title>table_with_NestedLoop</title>
+	<title>Part3:table_with_NestedLoop</title>
 	
 	
   </head>
