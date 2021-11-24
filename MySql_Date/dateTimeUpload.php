f6e51e764643a9d4c83ee18d96240fbf40e2afd3
sghadai2997 <89852365+sghadai2997@users.noreply.github.com>
2021-11-24 09:41:30 -0500

    Update dateTimeUpload.php

--- MySql_Date/dateTimeUpload.php
+++ MySql_Date/dateTimeUpload.php
@@ -6,14 +6,15 @@ public function checkSubmit()
 {
 		
 	$dateT=$_POST['date_time'];
-	$statement = $_POST['date_time'];
+	//$statement = $_POST['date_time'];
 	//string is going to be the substring function, 0 is for start, there is 12 months in the year.
-	$year = substr($statement, 0, -12);
-	$month = substr($statement, 5, -9);
-	$day = substr($statement, 8, -6);
-	$hour = substr($statement, 11, -3);
-	$minute = substr($statement, 14);
-	$noteTime = gmmktime($hour, $minute, 30, $month, $day, $year);
+	//$year = substr($statement, 0, -12);
+	//$month = substr($statement, 5, -9);
+	//$day = substr($statement, 8, -6);
+	//$hour = substr($statement, 11, -3);
+	//$minute = substr($statement, 14);
+	//$noteTime = gmmktime($hour, $minute, 30, $month, $day, $year);
+	$my_date = date("Y-m-d H:i:s");
 
 		//$dateT=$_POST['date_time'];
 		$note=$_POST['notes'];
@@ -31,7 +32,7 @@ public function checkSubmit()
 				$sql = "INSERT INTO dates (date_time, notes) VALUES(:DateTime, :Note)";
 			/* THESE BINDINGS ARE LATER INJECTED INTO THE SQL STATEMENT THIS PREVENTS AGAIN SQL INJECTIONS */
 			$bindings = [
-					[':DateTime',$noteTime,'int'],
+					[':DateTime',$my_date,'int'],
 					[':Note', $note,'str'],
 					//[':submit'.$note,'str'],
 				];
